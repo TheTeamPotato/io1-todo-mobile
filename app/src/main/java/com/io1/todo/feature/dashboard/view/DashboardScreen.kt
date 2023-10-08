1171b066fa7652aac44579ba17adf0cf80f6c71b
isoguzay <isoguzay@gmail.com>
2023-10-08 13:19:33 +0200

    #4 - Update dashboard screen

--- app/src/main/java/com/io1/todo/feature/dashboard/view/DashboardScreen.kt
+++ app/src/main/java/com/io1/todo/feature/dashboard/view/DashboardScreen.kt
@@ -1,7 +1,14 @@
 package com.io1.todo.feature.dashboard.view
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
+fun DashboardScreenRoute() {
+    DashboardScreen()
+}
+
+@Composable
 fun DashboardScreen() {
+    Text(text = "DashboardScreen")
 }
