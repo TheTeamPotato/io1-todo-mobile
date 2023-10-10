c8968c5e6b2d3cad3fa2c8cfe3d9d9c0a7851d8a
Kayra Özenalp <ozenalpkayraenez@gmail.com>
2023-10-10 23:16:18 +0300

    #7 - Add provideDataStoreRepository to AppModule

--- app/src/main/java/com/io1/todo/di/AppModule.kt
+++ app/src/main/java/com/io1/todo/di/AppModule.kt
@@ -1,4 +1,21 @@
 package com.io1.todo.di
 
+import android.content.Context
+import com.io1.data.datastore.DataStoreRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideDataStoreRepository(
+        @ApplicationContext context: Context
+    ) = DataStoreRepository(context = context)
+
 }
